1c416e05f799aa6085640deff332ded4646934f5
Jxr2 <3212481821@qq.com>
2026-01-04 02:51:29 +0800

    final fix tests9.rs with export_name

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -36,7 +36,7 @@ extern "Rust" {
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
-    #[no_mangle]
+    #[export_name="my_demo_function"]
     pub fn my_demo_function(a: u32) -> u32 {
         a
     }
